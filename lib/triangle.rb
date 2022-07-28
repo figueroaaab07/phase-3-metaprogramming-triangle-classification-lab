68866091bfc73459f0942b8b50a0d733a4d18515
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-07-27 18:20:44 -0700

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @sides = [side_1, side_2, side_3].sort
+  end
+
+
+  def kind
+    a, b, c = @sides
+    if a <= 0 || a + b <= c
+      # begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      # end
+    end
+    return :equilateral if a == c
+    return :isosceles if a == b || b == c
+    return :scalene
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+    def message
+      "Triangle with no size or with negative side or violating triangle inequality"
+    end
+  end
 end
